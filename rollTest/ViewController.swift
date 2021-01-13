35874d2b0653e16e55585f6946dd8a071ac710f3
botanPro <69993268+botanPro@users.noreply.github.com>
2021-01-13 17:50:00 +0300

    Initial Commit

---
+++ rollTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rollTest
+//
+//  Created by Botan Amedi on 13/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
